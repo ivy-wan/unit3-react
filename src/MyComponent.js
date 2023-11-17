b5b79820df1e7d8c8d036acef56c3ea2a61883ac
ivy-wan <ivywan@bergen.org>
2023-11-17 10:02:23 -0500

    Class code

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>My name is Ms. Wang!</h1>
+        </div>
+    );
+}
+
+export default MyComponent;
